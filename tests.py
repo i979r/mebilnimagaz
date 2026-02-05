7e92a5a2a4ed6372d32ee8deb432f9ea16dbc713
i979r <irfanmun2011@mail.ru>
2026-02-05 17:27:38 +0300

    мой CI

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 def test_math_operation():
     x = 10
     y = 5
-    assert x + y == 15
+    assert x + y == 5
     assert x - y == 5
+    assert 2 + 2 == 5
 
 def def_string_operation():
     word = 'GitHub'
